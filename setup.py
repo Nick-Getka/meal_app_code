eab8b00f12553987b1e7abe4181ae34a15258d37
Nick Getka <ngetka@getka-xps13-fedora.home>
2017-07-27 20:19:17 -0400

    finishing the setup.py for now

--- setup.py
+++ setup.py
@@ -20,36 +20,32 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-
-
 from setuptools import setup
 
 try:
     import meal_planner
 except ImportError:
-    meal_planner = False
+    meal_planner = None
 
-print "Test"
-
-if not meal_planner:
-    description = open(README.rst).read()
+if meal_planner:
+    description = open("./README.rst").read()
     version = meal_planner.VERSION
 
     requirements = [
-        line.strip() for line in open(".requirements.txt")
+        line.strip() for line in open("./requirements.txt")
         ]
 
-
-
     setup(
-    name = "meal_planner",
-    version = version,
-    url = "https://github.com/Nick-Getka/meal_app_code",
-    license = "MIT License",
-    author = "Nick Getka",
-    author_email = "nicholas.getka@gmail.com",
-    description = "A smart weekly meal planning app and food organizer",
-    long_description = description,
-    packages = ["emsm", "emsm.core", "emsm.core.lib", "emsm.plugins"],
-    install_requires = requirements,
+        name = "meal_planner",
+        version = version,
+        url = "https://github.com/Nick-Getka/meal_app_code",
+        license = "MIT License",
+        author = "Nick Getka",
+        author_email = "nicholas.getka@gmail.com",
+        description = "A smart weekly meal planning app and food organizer",
+        long_description = description,
+        packages = ["meal_planner"],
+        install_requires = requirements,
     )
+else:
+    print "ERROR: During Setup"
